38cafccab0eb6566f089c4a4e6e76bf4209df495
dir-FY25tomitanao <nao.tomita.r357@dirbato.co.jp>
2025-05-23 16:44:51 +0900

    Lesson2終わり

---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Test {
+
+      main2{
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
+    }
+
+}
---
+++ Sample/src/sample/Variable.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        
+        String str1;
+        str1 = "abc";
+        
+        // printInで出力
+        System.out.println(str1);
+        System.out.println(str1);
+        System.out.println(str1);
+        System.out.println();
+        
+        // printで出力
+        System.out.print(str1);
+        System.out.print(str1);
+        System.out.print(str1);
+        
+    }
+
+}
